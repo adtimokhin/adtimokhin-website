3435d6f3871d7c2ea36ad7e0b66b042a12d31db4
adtimokhin <adtimokhin@gmail.com>
2022-07-30 15:46:27 +0600

    I was  playing around with detecting position of a cursor relative to the origin of the scene. I have decided to use matrix multiplication for that. Code in ProjectSelectionCanvas element is trash. I added a matrix class that seems to work, though I need to test it.

---
+++ math/matrix.js
@@ -0,0 +1,84 @@
+class Matrix2D {
+  constructor(rows) {
+    this.rows = rows;
+    this.numberOfRows = rows.length;
+    this.numberOfColumns = rows[0].length;
+  }
+
+  getRow(index) {
+    if (index >= this.rows.length) {
+      return Error("Out of bound");
+    }
+
+    if (index < 0) {
+      return Error("Out of bound");
+    }
+
+    return this.rows[index];
+  }
+
+  getColumn(index) {
+    if (index >= this.rows[0].length) {
+      return Error("Out of bound");
+    }
+
+    if (index < 0) {
+      return Error("Out of bound");
+    }
+
+    const column = [];
+    for (var i = 0; i < this.rows.length; i++) {
+      column.push(this.rows[i][index]);
+    }
+
+    return column;
+  }
+
+  multiply(secondMatrix) {
+    if (this.numberOfColumns != secondMatrix.numberOfRows) {
+      return Error("Math Error: matrices have incompatable dimensions");
+    }
+
+    // creating a result matrix with dummy values
+    const resultDummyMatrix = [];
+    for (var i = 0; i < this.numberOfRows; i++) {
+      const row = [];
+      for (var j = 0; j < secondMatrix.numberOfColumns; j++) {
+        row.push[0];
+      }
+      resultDummyMatrix.push(row);
+    }
+
+    function multiplyArrays(arr1, arr2) {
+      var result = 0;
+      for (let i = 0; i < arr1.length; i++) {
+        result += arr1[i] * arr2[i];
+      }
+
+      return result;
+    }
+
+    // calculating values to store in the result matrix
+    for (var i = 0; i < this.numberOfRows; i++) {
+      const row = this.getRow(i);
+      for (var j = 0; j < secondMatrix.numberOfColumns; j++) {
+        const column = secondMatrix.getColumn(j);
+        resultDummyMatrix[i][j] = multiplyArrays(row, column);
+      }
+    }
+
+    // Finding rows
+    const rows = [];
+    for (let i = 0; i < resultDummyMatrix.length; i++) {
+      const row = resultDummyMatrix[i];
+      rows.push(row);
+    }
+
+    const resultMatrix = new Matrix2D(rows);
+    return resultMatrix;
+  }
+
+
+}
+
+export default Matrix2D
--- src/components/threeJSContent/ProjectSelectionCanvas.jsx
+++ src/components/threeJSContent/ProjectSelectionCanvas.jsx
@@ -2,6 +2,8 @@ import { useEffect } from "react";
 import * as THREE from "three";
 import { GLTFLoader } from "three/examples/jsm/loaders/GLTFLoader.js";
 
+import Matrix2D from "../../../math/matrix";
+
 const gltfloader = new GLTFLoader();
 
 function ProjectSelectionCanvas(props) {
@@ -12,16 +14,76 @@ function ProjectSelectionCanvas(props) {
     const scene = new THREE.Scene();
     // making a basic camera
     const camera = new THREE.PerspectiveCamera(
-      50,
+      45,
       window.innerWidth / window.innerHeight,
       1,
       1000
     );
 
-    camera.position.z = 1.5;
+    // camera.rotation.x = - Math.PI / 3;
+
+    var minY = 100;
+    var maxY = -100;
+
+    var minX = 100;
+    var maxX = -100;
+
+    function showA(event) {
+      // Calculating Camera positon
+      const cameraX = camera.position.x;
+      const cameraY = camera.position.y;
+      const cameraZ = camera.position.z;
+
+      const cameraYAngle = camera.rotation.y;
+      const cameraXAngle = camera.rotation.x;
+      const cameraZAngle = camera.rotation.z;
+
+      // Step one: Connvert the client's coordinates to standart form
+
+      var clientX = 2 * (event.clientX / window.innerWidth) - 1;
+      var clientY = 1 - 2 * (event.clientY / window.innerHeight);
+
+      // Applying rotation in Y-plane:
+      var mouseX = Math.cos(cameraYAngle) * clientX;
+      var mouseY = clientY;
+      var mouseZ = -Math.sin(cameraYAngle) * clientX;
+
+      // Applying rotation in X-plane:
+      mouseX = mouseX;
+      mouseY = Math.cos(cameraXAngle) * mouseY;
+      mouseZ = Math.sin(cameraXAngle) * clientY;
+
+      // Applying rotation in Z-plane:
+      const oldX = mouseX;
+      const oldY = mouseY;
+      mouseX = Math.cos(cameraZAngle) * oldX + Math.sin(cameraZAngle) * oldY;
+      mouseY = Math.sin(cameraZAngle) * oldX + Math.cos(cameraZAngle) * oldY;
+
+      if (mouseY > maxY) {
+        maxY = mouseY;
+      }
+
+      if (mouseY < minY) {
+        minY = mouseY;
+      }
+
+      if (mouseX > maxX) {
+        maxX = mouseX;
+      }
+
+      if (mouseX < minX) {
+        minX = mouseX;
+      }
+
+      console.log(`Mouse coordinates: (${mouseX}, ${mouseY} , ${mouseZ})`);
+      console.log(`distance in y-plane: ${maxY - minY}`);
+      console.log(`distance in x-plane: ${maxX - minX}`);
+    }
 
     // Setting up a canvas and a renderer
     const canvas = document.getElementById(canvasId); // Canvas where we draw the scene on the user's screen
+
+    canvas.addEventListener("mousemove", showA);
 
     const renderer = new THREE.WebGLRenderer({
       canvas: canvas,
@@ -45,7 +107,16 @@ function ProjectSelectionCanvas(props) {
 
     scene.add(boxMesh);
 
+    // Helper
+    const size = 1000;
+    const divisions = 1000;
+
+    const gridHelper = new THREE.GridHelper(size, divisions);
+    scene.add(gridHelper);
+
     // Blender Models
+
+    // Sample model
     gltfloader.load("static/iMadeADonut.gltf", (gltf) => {
       scene.add(gltf.scene);
     });
@@ -56,6 +127,7 @@ function ProjectSelectionCanvas(props) {
         boxMesh.rotation.x += 0.01;
         boxMesh.rotation.y += 0.01;
       }
+
       renderer.render(scene, camera);
       window.requestAnimationFrame(animate);
     };
