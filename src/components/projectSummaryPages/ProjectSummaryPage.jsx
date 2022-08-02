74166527a424c51e852dea6e2302cc2d4ca93ab2
adtimokhin <adtimokhin@gmail.com>
2022-08-02 17:52:02 +0600

    I updated information on wds datapage. I am still not sure that it can be considered a final version of the text

--- src/components/projectSummaryPages/ProjectSummaryPage.jsx
+++ src/components/projectSummaryPages/ProjectSummaryPage.jsx
@@ -5,11 +5,11 @@ import Footer from "../footer/Footer";
 import Epigraph from "./Epigraph";
 
 function getDivColours(colourScheme) {
-  switch (colourScheme){
+  switch (colourScheme) {
     case "wds":
-      return "bg-wds-300"
+      return "bg-wds-300";
     default:
-      return "bg-gray-300"
+      return "bg-gray-300";
   }
 }
 
@@ -31,19 +31,21 @@ function ProjectSummaryPage(props) {
   }
 
   return (
-    <div className={getDivColours(colourScheme)}>
-      <FullGridScreen>
-        <ProjectLogo
-          image={content.imageScr}
-          projectName={content.projectName}
-        />
-        <Epigraph
-          quote={content.quote}
-          author={content.quoteAuthor}
-          colourScheme={colourScheme}
-        />
-        {paragraphs}
-      </FullGridScreen>
+    <div>
+      <div className={"pb-20 " + getDivColours(colourScheme)}>
+        <FullGridScreen>
+          <ProjectLogo
+            image={content.imageScr}
+            projectName={content.projectName}
+          />
+          <Epigraph
+            quote={content.quote}
+            author={content.quoteAuthor}
+            colourScheme={colourScheme}
+          />
+          {paragraphs}
+        </FullGridScreen>
+      </div>
       <Footer />
     </div>
   );
